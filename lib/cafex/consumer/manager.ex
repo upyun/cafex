d094e86ed0c56c231c98c5d67c30c78f04a0e586
belltoy <belltoy@gmail.com>
2015-11-06 14:56:29 +0800

    Fixed ConsumerCoordinatorNotAvailableCode when send ConsumerMetadataRequest
    > The offsets for a given consumer group are maintained by a specific broker called the offset coordinator. i.e., a consumer needs to issue its offset commit and fetch requests to this specific broker. It can discover the current offset coordinator by issuing a consumer metadata request.
    > The broker returns this error code for consumer metadata requests or offset commit requests if the offsets topic has not yet been created.
    
    See
    * [Kafka Error Codes](https://cwiki.apache.org/confluence/display/KAFKA/A+Guide+To+The+Kafka+Protocol#AGuideToTheKafkaProtocol-ErrorCodes)
    * [issue on StackOverflow](http://stackoverflow.com/questions/28513744/kafka-0-8-2-consumermetadatarequest-always-return-consumercoordinatornotavailabl)

--- lib/cafex/consumer/manager.ex
+++ lib/cafex/consumer/manager.ex
@@ -552,16 +552,38 @@ defmodule Cafex.Consumer.Manager do
   defp get_coordinator(group, brokers) do
     {:ok, pid} = broker_connect(brokers)
     request = %ConsumerMetadata.Request{consumer_group: group}
-    reply = case Connection.request(pid, request, ConsumerMetadata) do
-      {:ok, %{error: :no_error, coordinator_host: host, coordinator_port: port}} ->
-        {:ok, {host, port}}
-      {:ok, %{error: code}} ->
-        {:error, code}
-      {:error, reason} ->
-        {:error, reason}
-    end
+    reply = try_fetch_consumer_metadata(pid, request)
     Connection.close(pid)
     reply
+  end
+
+  defp try_fetch_consumer_metadata(conn, request) do
+    try_fetch_consumer_metadata(conn, request, 10)
+  end
+  defp try_fetch_consumer_metadata(_conn, _request, 0) do
+    {:error, :consumer_coordinator_not_available}
+  end
+  defp try_fetch_consumer_metadata(conn, request, retries) when is_integer(retries) and retries > 0 do
+    case Connection.request(conn, request, ConsumerMetadata) do
+      {:ok, %{error: :no_error, coordinator_host: host, coordinator_port: port}} ->
+        {:ok, {host, port}}
+      {:ok, %{error: :consumer_coordinator_not_available}} ->
+        # We have to send a ConsumerMetadataRequest and retry with back off if
+        # you receive a ConsumerCoordinatorNotAvailableCode returned as an
+        # error.
+        # See
+        # [Kafka Error Codes](https://cwiki.apache.org/confluence/display/KAFKA/A+Guide+To+The+Kafka+Protocol#AGuideToTheKafkaProtocol-ErrorCodes)
+        # [issue on StackOverflow](http://stackoverflow.com/questions/28513744/kafka-0-8-2-consumermetadatarequest-always-return-consumercoordinatornotavailabl)
+        Logger.warn "Try fetch consumer metadata error: :consumer_coordinator_not_available, retry"
+        :timer.sleep(2000)
+        try_fetch_consumer_metadata(conn, request, retries - 1)
+      {:ok, %{error: code}} ->
+        Logger.error "Try fetch consumer metadata error: #{inspect code}"
+        {:error, code}
+      {:error, reason} ->
+        Logger.error "Try fetch consumer metadata error: #{inspect reason}"
+        {:error, reason}
+    end
   end
 
   defp broker_connect(brokers), do: broker_connect(brokers, :no_broker)
