c2bf83e14afe3563ebc71133dd082503f5d3c4d2
belltoy <belltoy@gmail.com>
2016-01-12 17:39:52 +0800

    Update Elixir to 1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cafex.Mixfile do
   def project do
     [app: :cafex,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps,
