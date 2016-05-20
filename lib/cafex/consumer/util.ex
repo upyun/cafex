a2356560f57ab7282eacad7c56295f1b05b922f9
belltoy <belltoy@gmail.com>
2016-05-20 11:09:36 +0800

    Add test cases for consumer util module

--- lib/cafex/consumer/util.ex
+++ lib/cafex/consumer/util.ex
@@ -1,5 +1,23 @@
 defmodule Cafex.Consumer.Util do
 
+  @doc """
+  Decode partitioins string stored outside.
+
+  ## Example
+
+      iex> decode_partitions(nil)
+      []
+      iex> decode_partitions("")
+      []
+      iex> decode_partitions("[1,2,3,4,5]")
+      [1,2,3,4,5]
+      iex> decode_partitions("[1,2,3,]")
+      [1,2,3]
+      iex> decode_partitions("[1,2,3")
+      [1,2,3]
+      iex> decode_partitions("1,2,3")
+      [1,2,3]
+  """
   def decode_partitions(nil), do: []
   def decode_partitions(""), do: []
   def decode_partitions(value) do
@@ -10,6 +28,18 @@ defmodule Cafex.Consumer.Util do
           |> Enum.map(&String.to_integer/1)
   end
 
+  @doc """
+  Encode partitions list to string.
+
+  ## Example
+
+      iex> encode_partitions(nil)
+      ""
+      iex> encode_partitions([])
+      "[]"
+      iex> encode_partitions([1,2,3,4])
+      "[1,2,3,4]"
+  """
   def encode_partitions(nil), do: ""
   def encode_partitions(partitions) do
     "[#{ partitions |> Enum.map(&Integer.to_string/1) |> Enum.join(",") }]"
